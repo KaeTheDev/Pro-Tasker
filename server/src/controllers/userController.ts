69bf988b74f5cc90e334b95a392e214bb9d9a06e
KaeTheDev <kaethedev@gmail.com>
2026-01-19 12:02:44 -0500

    updated server file to include a home test route and updated jwt token function in userController, added a secret JWT token key and tested routes in postman

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,8 +4,11 @@ import jwt from 'jsonwebtoken';
 
 // Generate JWT
 const generateToken = (id: string) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET!, { expiresIn: '30d' });
-};
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined in .env");
+    }
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+  };  
 
 // Register new user
 // POST /api/users/register
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -8,8 +9,14 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+// Routes
+app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Server is running! 🚀')
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, ()=> {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
