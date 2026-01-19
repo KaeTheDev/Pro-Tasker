31e6dbc6a1b4ecd93d34c1f7ae122d58e0449b45
KaeTheDev <kaethedev@gmail.com>
2026-01-19 11:30:28 -0500

    Updated User model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,12 +1,33 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new mongoose.Schema(
+interface IUser extends mongoose.Document {
+    name: string;
+    email: string;
+    password: string;
+    matchPassword: (enteredPassword: string) => Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
     {
         name: { type: String, required: true },
         email: { type:  String, required: true, unique: true },
         password: { type: String, required: true }
     },
-    { timestamps: true }
-);
+    { timestamps: true });
 
-export default mongoose.model('User', userSchema);
+    // Run function before any document is saved and hash password
+    userSchema.pre('save', async function(this: any) {
+        if(!this.isModified('password')) return;
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    });
+
+    // Compare entered password with hashed password
+    userSchema.methods.matchPassword = async function(enteredPassword: string) {
+        return await bcrypt.compare(enteredPassword, this.password);
+    };
+
+    const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
