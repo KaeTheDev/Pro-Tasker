7618b7e135869918907f72d650a0a4eec9ecd9fd
KaeTheDev <kaethedev@gmail.com>
2026-01-19 17:34:07 -0500

    Added tasks routes to server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
     res.send('Server is running! 🚀')
