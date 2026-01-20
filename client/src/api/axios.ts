c93a74a4fc4136b2ad3e8047e428659f0f58ffd8
KaeTheDev <kaethedev@gmail.com>
2026-01-19 21:00:28 -0500

    Updated axios.ts file

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -7,4 +7,20 @@ const api = axios.create({
     },
 });
 
+// intercept requests to automatically attach token
+api.interceptors.request.use((config) => {
+    // Get user from localStorage
+    const user = localStorage.getItem("user");
+    if (user) {
+        // Extract JWT token
+      const token = JSON.parse(user).token;
+       // Ensure headers object exists
+      if (config.headers)
+        // Attach token
+         config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    // Return the modified config to Axios
+    return config;
+  });
+
 export default api;
